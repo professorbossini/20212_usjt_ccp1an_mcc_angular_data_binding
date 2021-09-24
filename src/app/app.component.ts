5985e4df67d025736988e5ec71d4c1a357614874
Rodrigo Bossini <professorbossini@gmail.com>
2021-09-23 21:05:05 -0300

    Encerramos a apostila 08

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nome = 'José'
+  nome: string
   idade = 20
+
+  adicionar (input) {
+    console.log(input.value)
+    this.nome = input.value
+  }
+
+  alterarNome (evento){
+    console.log(evento.target.value)
+    this.nome = evento.target.value
+  }
+
+
   lancarDado(){
     return Math.floor((Math.random() * 6)) + 1
   }
